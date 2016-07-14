230a7ea534bb640ade91c1e88159ae93b296d643
NMNappi22 <NMNappi22@gmail.com>
2016-07-13 15:01:01 -1000

    help

---
+++ js/results.js
@@ -0,0 +1,56 @@
+window.onload = initresults
+
+function initresults(){
+	function letterGrade(score, maximumLoserScore){
+		if(score >= 130 && score <= 140){
+			document.getElementById('score').innerHTML('A')
+		}
+
+		if(score >= 100 && score < 130){
+			document.getElementById('score').innerHTML('B')
+		}
+
+		if(score >= 85 && score < 100){
+			document.getElementById('score').innerHTML('C')
+		}
+
+		if(score >= 65 && score < 85){
+			document.getElementById('score').innerHTML('D')
+		}
+
+		if(score >= 0  && score < 65){
+			document.getElementById('score').innerHTML('This score should not be possible')
+		}
+
+		//split
+
+		if(score > 140 && score < 155){
+			document.getElementById('score').innerHTML('B')
+		}
+
+		if(score > 155 && score < 165){
+			document.getElementById('score').innerHTML('C')
+		}
+
+		if(score > 165 && score < 175){
+			document.getElementById('score').innerHTML('D')
+		}
+
+		if(score > 175 && score < 185){
+			document.getElementById('score').innerHTML('F')
+		}
+
+		if(score >= 185){
+			document.getElementById('score').innerHTML('Go apply for a Guinness world record..')
+		}
+
+	}
+
+var hoverScore = document.getElementById('theMoney');
+hoverScore.addEventListener('click', letterGrade);
+	
+	
+
+letterGrade(140)
+
+}
