5b7d2b7b79924fe939ee6479b90975b4e08ea17d
NMNappi22 <NMNappi22@gmail.com>
2016-07-14 14:01:42 -1000

    helo

--- js/results.js
+++ js/results.js
@@ -3,54 +3,63 @@ window.onload = initresults
 function initresults(){
 	function letterGrade(score, maximumLoserScore){
 		if(score >= 130 && score <= 140){
-			document.getElementById('score').innerHTML('A')
+			document.getElementById('score').innerHTML ='A'
 		}
 
 		if(score >= 100 && score < 130){
-			document.getElementById('score').innerHTML('B')
+			document.getElementById('score').innerHTML = 'B'
 		}
 
 		if(score >= 85 && score < 100){
-			document.getElementById('score').innerHTML('C')
+			document.getElementById('score').innerHTML = 'C'
 		}
 
 		if(score >= 65 && score < 85){
-			document.getElementById('score').innerHTML('D')
+			document.getElementById('score').innerHTML = 'D'
 		}
 
 		if(score >= 0  && score < 65){
-			document.getElementById('score').innerHTML('This score should not be possible')
+			document.getElementById('score').innerHTML = 'This score should not be possible'
 		}
 
 		//split
 
 		if(score > 140 && score < 155){
-			document.getElementById('score').innerHTML('B')
+			document.getElementById('score').innerHTML = 'B'
 		}
 
 		if(score > 155 && score < 165){
-			document.getElementById('score').innerHTML('C')
+			document.getElementById('score').innerHTML = 'C'
 		}
 
 		if(score > 165 && score < 175){
-			document.getElementById('score').innerHTML('D')
+			document.getElementById('score').innerHTML = 'D'
 		}
 
 		if(score > 175 && score < 185){
-			document.getElementById('score').innerHTML('F')
+			document.getElementById('score').innerHTML = 'F'
 		}
 
 		if(score >= 185){
-			document.getElementById('score').innerHTML('Go apply for a Guinness world record..')
+			document.getElementById('score').innerHTML = 'Go apply for a Guinness world record..'
 		}
 
 	}
 
-var hoverScore = document.getElementById('theMoney');
-hoverScore.addEventListener('click', letterGrade);
-	
-	
+letterGrade();
 
-letterGrade(140)
+var score = randomScore();
+letterGrade(score);
+
+function randomScore(){
+	// 60 is the minimum, 200 is the maximum
+	var randomNumber = Math.floor(Math.random() * (200 - 80 + 1)) + 80;
+	return randomNumber;
+}
+
+randomScore();
+
+document.getElementById('wpm').innerHTML = score;
+
 
 }
